2844677d1c9cce39c9faaf9406476e0fa09b9b0e
Allyson1602 <aathyrson@yahoo.com.br>
2023-09-24 16:58:47 -0300

    chore: definição da estrutura da page found

---
+++ src/lib/components/chip.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import Icon from "@iconify/svelte";
+
+    export let text: string;
+    export let color: string;
+
+    const handleRemoveChip = () => {
+        //
+    };
+</script>
+
+<button on:click={handleRemoveChip}>
+    <div class={`${color}` + " inline-block relative py-1 px-3 rounded-2xl text-sm"}>
+        <span class="absolute right-[-5px] top-[-5px]">
+            <Icon icon="ph:x-circle-fill" width="20" height="20" class="text-red-900" />
+        </span>
+        
+        <p>{text}</p>
+    </div>
+</button>
--- src/routes/found/+page.svelte
+++ src/routes/found/+page.svelte
@@ -1,15 +1,16 @@
-<script lg="ts">
+<script lang="ts">
+	import Chip from "$lib/components/chip.svelte";
 	import Menu from "$lib/components/menu.svelte";
 	import Icon from "@iconify/svelte";
 
 	let foodValue = "Abobrinha madura";
 </script>
 
-<div class="h-screen flex flex-col">
+<div class="h-screen flex flex-col md:flex-row">
 	<Menu />
 	
 	<div class="flex flex-col justify-between grow">
-		<div class="grid grid-cols-1 md:grid-cols-2 p-5">
+		<div class="grid grid-cols-1 p-5 md:grid-cols-2 md:gap-10">
 			<div>
 				<div class="flex flex-col">
 					<label for="food-field" class="text-sm text-neutral-700">O que tem pra comer?</label>
@@ -40,16 +41,30 @@
 				</div>
 			</div>
 		
-			<div class="mt-4">
-				<h3 class="text-xl text-orange-400 font-bold">Selecionados:</h3>
-
+			<div class="flex flex-col justify-between mt-4">
 				<div>
+					<h3 class="text-xl text-orange-400 font-bold">Selecionados:</h3>
+	
+					<div class="flex flex-wrap gap-x-2 gap-y-2 mt-4 md:justify-start">
+						<Chip text="laranja" color="bg-orange-400" />
+						<Chip text="laranja" color="bg-orange-400" />
+						<Chip text="laranja" color="bg-orange-400" />
+						<Chip text="laranja" color="bg-orange-400" />
+						<Chip text="laranja" color="bg-orange-400" />
+						<Chip text="laranja" color="bg-orange-400" />
+						<Chip text="laranja" color="bg-orange-400" />
+						<Chip text="laranja" color="bg-orange-400" />
+						<Chip text="laranja" color="bg-orange-400" />
+					</div>
+				</div>
 
+				<div class="flex justify-center mt-3">
+					<button class="py-2 px-10 bg-orange-500 text-orange-50 rounded-lg">pegar receitas</button>
 				</div>
 			</div>
 		</div>
 		
-		<div class="h-16 relative bg-blue-400 bg-kitchen-utensils">
+		<div class="h-16 relative bg-blue-400 bg-kitchen-utensils md:hidden">
 			<div class="w-full flex justify-center mt-[-16px] absolute left-0 top-0">
 				<button>
 					<Icon icon="ph:hand-grabbing-fill" width="32" height="32" class="text-orange-400" />
