30cfc3607517990bd658598b59aebcbb51aca81c
Allyson1602 <aathyrson@yahoo.com.br>
2023-09-26 10:14:29 -0300

    chore: configuração para background dos chips dinâmica

--- src/lib/components/chip.svelte
+++ src/lib/components/chip.svelte
@@ -10,11 +10,11 @@
 </script>
 
 <button on:click={handleRemoveChip}>
-    <div class={`${color}` + " inline-block relative py-1 px-3 rounded-2xl text-sm"}>
+    <div class={color + " inline-block relative py-1 px-3 rounded-2xl text-sm"}>
         <span class="absolute right-[-5px] top-[-5px]">
             <Icon icon="ph:x-circle-fill" width="20" height="20" class="text-red-900" />
         </span>
-        
+
         <p>{text}</p>
     </div>
 </button>
--- src/routes/found/+page.svelte
+++ src/routes/found/+page.svelte
@@ -3,63 +3,112 @@
 	import Menu from "$lib/components/menu.svelte";
 	import Icon from "@iconify/svelte";
 
-	let foodValue = "Abobrinha madura";
+	let foodValue = "";
+	let foundedFoods: string[] = [];
+	let selectedFoods: string[] = ["manga", "laranja", "sal", "macarrão", "trigo", "miojo", "água", "tomate", "pêra", "salsisha", "alface", "farinha", "beterraba", "batata", "açafrão"];
+
+	let currentCodeColor = 100;
+	let currentNameColor = "orange";
+
+	const getRainbowColor = (): string => {
+		let currentColor = `bg-${currentNameColor}-${currentCodeColor}`;
+
+		if (currentCodeColor === 300 && currentNameColor === "orange") {
+			currentNameColor = "amber";
+		} else if (currentCodeColor === 100 && currentNameColor === "amber") {
+			currentNameColor = "yellow";
+		} else if (currentCodeColor === 300 && currentNameColor === "yellow") {
+			currentNameColor = "lime";
+		} else if (currentCodeColor === 100 && currentNameColor === "lime") {
+			currentNameColor = "green";
+		} else if (currentCodeColor === 300 && currentNameColor === "green") {
+			currentNameColor = "esmerald";
+		} else if (currentCodeColor === 100 && currentNameColor === "esmerald") {
+			currentNameColor = "teal";
+		} else if (currentCodeColor === 300 && currentNameColor === "teal") {
+			currentNameColor = "cyan";
+		} else if (currentCodeColor === 100 && currentNameColor === "cyan") {
+			currentNameColor = "sky";
+		} else if (currentCodeColor === 300 && currentNameColor === "sky") {
+			currentNameColor = "blue";
+		} else if (currentCodeColor === 100 && currentNameColor === "blue") {
+			currentNameColor = "orange";
+		} else {
+			if (
+				currentNameColor === "amber" ||
+				currentNameColor === "lime" ||
+				currentNameColor === "esmerald" ||
+				currentNameColor === "cyan" ||
+				currentNameColor === "blue"
+			) {
+				currentCodeColor -= 100;
+			} else {
+				currentCodeColor += 100;
+			}
+		}
+
+		return currentColor;
+	};
+
+	const handleInputFoodValue = (event: Event & {
+		currentTarget: EventTarget & HTMLInputElement;
+	}) => {
+		foundedFoods = ["Abobrinha", "Abobrinha Abobrinha", "Abobrinha", "Abobrinha Abobrinha Abobrinha", "Abobrinha Abobrinha", "Abobrinha"];
+	};
+
+	const handleBlurFoodValue = (event: FocusEvent & {
+		currentTarget: EventTarget & HTMLInputElement;
+	}) => {
+		foundedFoods = [];
+	};
 </script>
 
 <div class="h-screen flex flex-col md:flex-row">
 	<Menu />
 	
 	<div class="flex flex-col justify-between grow">
-		<div class="grid grid-cols-1 p-5 md:grid-cols-2 md:gap-10">
+		<div class="grid grid-cols-1 p-5 grow auto-rows-min md:grid-cols-2 md:gap-10">
 			<div>
-				<div class="flex flex-col">
+				<div class="flex flex-col max-w-[400px] mx-auto">
 					<label for="food-field" class="text-sm text-neutral-700">O que tem pra comer?</label>
-					<input id="food-field" bind:value={foodValue} class="shadow-lg text-base p-2 rounded border border-neutral-200" />
+					<input
+						id="food-field"
+						bind:value={foodValue}
+						on:blur={handleBlurFoodValue}
+						on:input={handleInputFoodValue}
+						class="shadow-lg text-base p-2 rounded border border-neutral-200"
+						placeholder="tomate, macarrão, leite..."
+					/>
 				</div>
 
-				<div class="rounded shadow-lg mt-4">
-					<ul class="px-8 py-3 flex flex-col gap-2">
-						<li class="pb-1 border-b border-neutral-200">
-							<button class="text-sm">Abobrinha</button>
-						</li>
-						<li class="pb-1 border-b border-neutral-200">
-							<button class="text-sm">Abobrinha Abobrinha</button>
-						</li>
-						<li class="pb-1 border-b border-neutral-200">
-							<button class="text-sm">Abobrinha</button>
-						</li>
-						<li class="pb-1 border-b border-neutral-200">
-							<button class="text-sm">Abobrinha Abobrinha Abobrinha</button>
-						</li>
-						<li class="pb-1 border-b border-neutral-200">
-							<button class="text-sm">Abobrinha Abobrinha</button>
-						</li>
-						<li class="pb-1">
-							<button class="text-sm">Abobrinha</button>
-						</li>
-					</ul>
-				</div>
+				{#if foundedFoods.length > 0}
+					<div class="rounded shadow-lg mt-4">
+						<ul class="px-8 py-3 flex flex-col gap-2">
+							{#each foundedFoods as food}
+								<li class="pb-1 border-b border-neutral-200 last:border-b-0">
+									<button class="text-sm">{food}</button>
+								</li>
+							{/each}
+						</ul>
+					</div>
+				{/if}
 			</div>
 		
-			<div class="flex flex-col justify-between mt-4">
+			<div class="flex flex-col mt-4">
 				<div>
 					<h3 class="text-xl text-orange-400 font-bold">Selecionados:</h3>
 	
-					<div class="flex flex-wrap gap-x-2 gap-y-2 mt-4 md:justify-start">
-						<Chip text="laranja" color="bg-orange-400" />
-						<Chip text="laranja" color="bg-orange-400" />
-						<Chip text="laranja" color="bg-orange-400" />
-						<Chip text="laranja" color="bg-orange-400" />
-						<Chip text="laranja" color="bg-orange-400" />
-						<Chip text="laranja" color="bg-orange-400" />
-						<Chip text="laranja" color="bg-orange-400" />
-						<Chip text="laranja" color="bg-orange-400" />
-						<Chip text="laranja" color="bg-orange-400" />
-					</div>
+					{#if selectedFoods.length > 0}
+						<div class="flex flex-wrap gap-x-2 gap-y-2 mt-4 md:justify-start">
+							{#each selectedFoods as food (food)}
+								<Chip text={food} color={getRainbowColor()} />
+							{/each}
+						</div>
+					{/if}
 				</div>
 
 				<div class="flex justify-center mt-3">
-					<button class="py-2 px-10 bg-orange-500 text-orange-50 rounded-lg">pegar receitas</button>
+					<a href="/recipes" class="w-80 bg-orange-400 rounded-full md:self-center p-2 text-3xl mx-auto font-semibold text-white/80 text-center">pegar receitas</a>
 				</div>
 			</div>
 		</div>
