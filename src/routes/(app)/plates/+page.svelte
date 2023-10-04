37d5f80a68914c53793359801243aec0c57436f8
Allyson1602 <aathyrson@yahoo.com.br>
2023-10-04 11:18:26 -0300

    refactor: criação de componente para manter carrosel da page plate

--- src/routes/(app)/plates/+page.svelte
+++ src/routes/(app)/plates/+page.svelte
@@ -1,21 +1,13 @@
 <script lang="ts">
 	import Chip from "$lib/components/chip.svelte";
-	import Icon from "@iconify/svelte";
-	import { Splide, SplideSlide, SplideTrack } from "@splidejs/svelte-splide";
 	import "@splidejs/svelte-splide/css";
 	import "@splidejs/svelte-splide/css";
-	import type { MoveEventDetail } from "@splidejs/svelte-splide/types";
-	import resolveConfig from "tailwindcss/resolveConfig";
-	import tailwindConfig from "../../../../tailwind.config";
 	import { plateList } from "$stores/plate";
-
-	const breakpoints = resolveConfig(tailwindConfig).theme?.screens as any;
-	const breakPointMd = parseInt(breakpoints.md.replace('px', ''));
+	import Carousel from "./carousel.svelte";
+	import type { MoveEventDetail } from "@splidejs/svelte-splide/types";
 
 	let currentPlate = 0;
 	let currentCategory = 0;
-
-    let screenSize: number;
 
 	const handleMoveCarousel = (event: CustomEvent<MoveEventDetail> | undefined) => {
 		if (event) {
@@ -33,44 +25,10 @@
 	<meta name="description" content="Listagem de pratos com ingredientes informados" />
 </svelte:head>
 
-<svelte:window bind:innerWidth={screenSize} />
-
 <div class="h-full relative">
 	<div class="flex flex-col justify-between gap-7 h-full mt-4 max-w-4xl">
 		<div>
-			<Splide hasTrack={false} on:move={handleMoveCarousel}>
-				<SplideTrack style="min-height: 15rem">
-					{#each $plateList as plate}
-						<SplideSlide>
-							<img src={plate.image} alt={plate.name} class="max-h-60 object-contain mx-auto my-2 rounded-xl shadow-md max-w-full md:max-w-lg" />
-						</SplideSlide>
-					{/each}
-				</SplideTrack>
-	
-				<ul class="splide__pagination invisible"></ul>
-	
-				<div class="splide__arrows">
-					<button class="splide__arrow splide__arrow--prev !bg-transparent -mt-36 md:-mt-0">
-						{#if breakPointMd >= screenSize}
-							<p class="text-orange-400 font-semibold">anterior</p>
-						{:else}
-							<button class="text-3xl !left-[0.5em] !lg:left-[1em]">
-								<Icon icon="ph:caret-double-right" />
-							</button>
-						{/if}
-					</button>
-
-					<button class="splide__arrow splide__arrow--next !bg-transparent -mt-36 md:-mt-0">
-						{#if breakPointMd >= screenSize}
-							<p class="text-orange-400 font-semibold">próximo</p>
-						{:else}
-							<button class="text-3xl !left-[0.5em] !lg:left-[1em]">
-								<Icon icon="ph:caret-double-right" />
-							</button>
-						{/if}
-					</button>
-				</div>
-			</Splide>
+			<Carousel onMove={handleMoveCarousel} />
 	
 			<div class="flex flex-col gap-4 items-center mt-4">
 				<p class="text-xl">{$plateList[currentPlate].name}</p>
---
+++ src/routes/(app)/plates/carousel.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+	import { plateList } from "$stores/plate";
+	import Icon from "@iconify/svelte";
+	import { Splide, SplideTrack, SplideSlide } from "@splidejs/svelte-splide";
+	import resolveConfig from "tailwindcss/resolveConfig";
+	import tailwindConfig from "$tailwind";
+	import type { MoveEventDetail } from "@splidejs/svelte-splide/types";
+
+    export let onMove: ((event: CustomEvent<MoveEventDetail> | undefined) => void) | undefined;
+
+    const breakpoints = resolveConfig(tailwindConfig).theme?.screens as any;
+    const breakPointMd = parseInt(breakpoints.md.replace('px', ''));
+    let screenSize: number;
+
+</script>
+
+<svelte:window bind:innerWidth={screenSize} />
+
+<Splide hasTrack={false} on:move={onMove}>
+    <SplideTrack style="min-height: 15rem">
+        {#each $plateList as plate}
+            <SplideSlide>
+                <img src={plate.image} alt={plate.name} class="max-h-60 object-contain mx-auto my-2 rounded-xl shadow-md max-w-full md:max-w-lg" />
+            </SplideSlide>
+        {/each}
+    </SplideTrack>
+
+    <ul class="splide__pagination invisible"></ul>
+
+    <div class="splide__arrows">
+        <button class="splide__arrow splide__arrow--prev !bg-transparent -mt-36 md:-mt-0">
+            {#if breakPointMd >= screenSize}
+                <p class="text-orange-400 font-semibold">anterior</p>
+            {:else}
+                <button class="text-3xl !left-[0.5em] !lg:left-[1em]">
+                    <Icon icon="ph:caret-double-right" />
+                </button>
+            {/if}
+        </button>
+
+        <button class="splide__arrow splide__arrow--next !bg-transparent -mt-36 md:-mt-0">
+            {#if breakPointMd >= screenSize}
+                <p class="text-orange-400 font-semibold">próximo</p>
+            {:else}
+                <button class="text-3xl !left-[0.5em] !lg:left-[1em]">
+                    <Icon icon="ph:caret-double-right" />
+                </button>
+            {/if}
+        </button>
+    </div>
+</Splide>
