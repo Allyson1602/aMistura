379739c472af1c98aea082b9380aa767c63bc229
Allyson Athyrson <allysonathyrson.work.gmail.com>
2023-12-19 12:08:56 -0300

    rating component (feat): create

---
+++ src/lib/components/rating.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import Icon from '@iconify/svelte';
+
+	export let rating: 1 | 2 | 3 | 4 | 5;
+
+	const iconProps = {
+		width: '20',
+		height: '20',
+		class: 'text-yellow-500'
+	};
+</script>
+
+<div class="flex">
+	<Icon icon={rating >= 1 ? 'ph:star-fill' : 'ph:star-light'} {...iconProps} />
+
+	<Icon icon={rating >= 2 ? 'ph:star-fill' : 'ph:star-light'} {...iconProps} />
+
+	<Icon icon={rating >= 3 ? 'ph:star-fill' : 'ph:star-light'} {...iconProps} />
+
+	<Icon icon={rating >= 4 ? 'ph:star-fill' : 'ph:star-light'} {...iconProps} />
+
+	<Icon icon={rating >= 5 ? 'ph:star-fill' : 'ph:star-light'} {...iconProps} />
+</div>
