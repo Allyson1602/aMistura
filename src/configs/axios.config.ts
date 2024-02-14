8bfba96aae6c1eca0d965dc27e0d44cdbf676592
Allyson Athyrson <allysonathyrson.work@gmail.com>
2024-02-14 20:39:49 -0300

    (refactor): test endpoint

--- src/configs/axios.config.ts
+++ src/configs/axios.config.ts
@@ -1,14 +1,15 @@
 import axios from 'axios';
-import md5 from 'md5';
 
 const instance = axios.create({
-	baseURL: 'https://1xvq0wt8i2.execute-api.sa-east-1.amazonaws.com/prod',
+	baseURL: 'http://localhost:3000/prod',
+	// baseURL: 'https://1xvq0wt8i2.execute-api.sa-east-1.amazonaws.com/prod',
 	headers: {
 		'Content-Type': 'application/json; charset=utf-8',
 		'Access-Control-Allow-Origin': '*',
+		'Access-Control-Allow-Methods': '*',
 		'Access-Control-Allow-Headers':
 			'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,Access-Control-Allow-Origin',
-		Authorization: md5(import.meta.env.VITE_AUTH_TOKEN)
+		Authorization: import.meta.env.VITE_AUTH_TOKEN
 	},
 	responseType: 'json'
 });
