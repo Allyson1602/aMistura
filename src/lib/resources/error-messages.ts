111a8a2038908c9308ae2485bc1f5294f43a5969
Allyson Athyrson <allysonathyrson.work@gmail.com>
2024-02-20 15:19:15 -0300

    (feat): Show error message in requests

---
+++ src/lib/resources/error-messages.ts
@@ -0,0 +1,12 @@
+export const errorMessages: string[] = [
+	'Ops! Parece que o pedido foi fritado demais. Tente novamente mais tarde.',
+	'Hummm, estamos sem ingredientes no momento. Tente outro pedido.',
+	'Uh oh! O forno da API está queimando tudo. Aguarde um pouco e tente novamente.',
+	'Desculpe, a louça suja está se acumulando. Limparemos e voltaremos em breve.',
+	'Parece que a receita está errada. Tente novamente com outros parâmetros.',
+	'O chef da API está de folga hoje. Volte amanhã para um novo pedido.',
+	'Ops! A torta caiu no chão. Tente outro pedido.',
+	'Hummm, a geladeira da API está vazia. Volte quando estivermos reabastecidos.',
+	'Uh oh! O gato da API derrubou o servidor. Aguarde um pouco e tente novamente.',
+	'Desculpe, o cozinheiro da API está com dor de barriga. Tente novamente mais tarde.'
+];
