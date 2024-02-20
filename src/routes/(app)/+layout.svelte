3e244adda35a0a7c9092b3452769b0b9c2f374ef
Allyson Athyrson <allysonathyrson.work@gmail.com>
2024-02-20 15:19:46 -0300

    (feat): getter a view about position toast

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -2,9 +2,19 @@
 	import Menu from '$lib/components/menu.svelte';
 	import '../styles.css';
 	import GrabBottom from '$lib/components/grab-bottom.svelte';
+	import { Toaster } from 'svelte-french-toast';
+	import { getBreakpoint } from '$lib/helpers/tailwind-breakpoint';
+	import { EBreakpoints } from '$lib/enums';
 
 	export let data;
+
+	const breakpointSm = getBreakpoint(EBreakpoints.SM);
+	let innerWidth: number;
 </script>
+
+<Toaster position={innerWidth > breakpointSm ? 'bottom-left' : 'bottom-center'} />
+
+<svelte:window bind:innerWidth />
 
 <div class="app">
 	<div class="h-screen flex flex-col justify-between">
